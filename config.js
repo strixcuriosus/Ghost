a4f2fd645c3795eabc56d1596914ee431ae20818
Ash Hoover <strixcuriosus@gmail.com>
2014-05-05 00:33:31 -0700

    Updated config for deployment

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'http://my-ghost-blog.com',
+        url: 'http://amhoover.azurewebsites.net',
 
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
@@ -37,7 +37,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            port: process.env.PORT //'2368'
         },
         paths: {
             contentPath: path.join(__dirname, '/content/')
